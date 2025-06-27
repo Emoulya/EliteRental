1a0115f8519b73388db69a0716fea530cb20bfc9
Emoulya <imtiyazsabda0628@gmail.com>
2025-06-28 00:17:10 +0700

    penyesuain component type button jadi submit

--- resources/views/components/buttons/primary-button.blade.php
+++ resources/views/components/buttons/primary-button.blade.php
@@ -1,5 +1,5 @@
 {{-- resources/views/components/buttons/primary-button.blade.php --}}
 <button
-    {{ $attributes->merge(['type' => 'button', 'class' => 'bg-gold text-navy font-bold px-5 py-2 rounded-lg hover:bg-yellow-500 transition duration-300']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-gold text-navy font-bold px-5 py-2 rounded-lg hover:bg-yellow-500 transition duration-300']) }}>
     {{ $slot }}
 </button>
